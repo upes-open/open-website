98f486f11356ae51a8be30e59deeba2f2b800c77
Poorva <poorvamaggu2003@gmail.com>
2023-01-26 16:19:00 +0530

    site

--- src/data/EventsData.js
+++ src/data/EventsData.js
@@ -1,26 +1,140 @@
 export const EventsData = [
     {
-        date: "23-12-2023",
-        name: "Uncovering RPA",
-        description: "We will be introducing the basics of RPA, including how it works, its benefits and applications, and how you can get started with it. You will also learn about the various fields that use RPA to automate monotonous tasks and why it is one of the most trending requirements in the workplace.",
-        type: "Offline",
-        imageURL: "https://i.postimg.cc/tCCZY2CH/Whats-App-Image-2023-01-19-at-10-38-36-PM.jpg",
-        registeration: 0,
+      name: "Uncovering RPA",
+      description: "Uncovering RPA and UiPath is a one-day workshop with the ambition of introducing one of the trendiest technologies in the tech gala. Robotic Process Automation is a technology that helps in furnishing the businesses with “bots” or software robots that pluck out the mundane tasks from the system and mimic the actions of a human worker to finish them. This is a rapidly evolving field which carries vast potential in transforming the way we work by improving efficiency. This event will start from the basic introduction of RPA, including details of its functioning, its benefits and applications. Learning about the various fields where RPA functions as a blooming industry will help one understand the importance automation carries in the future. Being equipped with the knowledge about the automation world will help in being industry ready. This workshop strives to go forward with the vision of making everybody automation ready. ",
+      imageURL:
+        "https://i.postimg.cc/tCCZY2CH/Whats-App-Image-2023-01-19-at-10-38-36-PM.jpg",
+      date: "23-01-2023",
+      registeration: "https://docs.google.com/forms/d/e/1FAIpQLSddNCLgkAE4zsWoGuWlrevNv5qzwaeLnhkVMqJGaB8gLwDw_g/viewform?usp=sf_link",
     },
     {
-        date: "23-12-2023",
-        name: "Uncovering RPA",
-        description: "We will be introducing the basics of RPA, including how it works, its benefits and applications, and how you can get started with it. You will also learn about the various fields that use RPA to automate monotonous tasks and why it is one of the most trending requirements in the workplace.",
-        type: "Offline",
-        imageURL: "https://i.postimg.cc/tCCZY2CH/Whats-App-Image-2023-01-19-at-10-38-36-PM.jpg",
-        registeration: 0,
+      name: "GIT Workshop and hands-on session",
+      description: "GIT is an open source distributed version control system that helps developers track changes in their code. In this workshop, participants will learn how to install and use GIT, how to create repositories, manage branches, and collaborate on projects using GIT. They will also learn how to use the command line interface and other advanced features. This workshop is ideal for beginning developers who want to learn about version control systems but also for experienced developers who want to become more proficient in using GIT.By attending this workshop, participants will walk away with a better understanding of GIT and its use in software development processes. They will be able to confidently use GIT in their own project or team environments to facilitate collaboration, version control, and open source development.",
+      imageURL: "https://ibb.co/TtX82pd",
+      date: "18-01-2023",
+      registeration: 0,
     },
     {
-        date: "23-12-2023",
-        name: "Uncovering RPA",
-        description: "We will be introducing the basics of RPA, including how it works, its benefits and applications, and how you can get started with it. You will also learn about the various fields that use RPA to automate monotonous tasks and why it is one of the most trending requirements in the workplace.",
-        type: "Online",
-        imageURL: "https://i.postimg.cc/tCCZY2CH/Whats-App-Image-2023-01-19-at-10-38-36-PM.jpg",
-        registeration: 0,
-    }
-];
+      name: "Dehashed",
+      description: "Dehashed is a one-day workshop with the aim to enlighten you about the new technology that can break the ecosystem. It renders as an amazing opportunity to get familiar with Blockchain and also get all your queries about NFTs, Crypto, Web 3.0 and Filecoin answered. It will not only strengthen your resume but will also help you to dive vastly in the sea of Blockchain Technology. It also offers an incomparable  chance of getting your contribution recognized and gaining your own digital asset. A thrilling opportunity to encounter a learning experience that can be helpful beyond words. Blockchain is the technology of today, making it an impactful tool for any new endures. Partnering with WEB CRUX along with being sponsored by the Filecoin Foundation, OPEN offers a fortuity like never before. Being vigilant about metaverse and cryptocurrencies is the need of the hour, offering it in such a way that they are not only easily understood but are also comprehended with their practical  utility is like a boon. Dehashed is just the place to retort all your questions.",
+      imageURL:
+        "https://i.postimg.cc/SNbp9Tk9/dehashed.jpg",
+      date: "07-11-2022",
+      registeration: 0,
+    },
+    {
+      name: "Registration Drive 2022-23",
+      description: "Our Community is pleased to declare that we welcome you to be an integral part of the core committee of trustees and more significantly, our family.",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1OszHe35ADNfCMxnPCawwziJ0C7L5uwDP",
+      date: "17-09-2022",
+      registeration: 0,
+    },
+    {
+      name: "Flutter Workshop",
+      description: "Flutter is a two-day long workshop with the aim of equipping all with the necessary tools to create hybrid apps. It not only helps in refining one’s technical skills but also helps to harness all the basic dialogues that need to be checked to learn to do hands-on projects. Also, it might just be the kickstart you needed to begin your mobile application development journey. This workshop aims to furnish you with all the skills you need to push all your boundaries and exceed your own expectations. It will help to learn everything from scratch and also provide hands-on experience on each and every aspect. We surround ourselves with mobile apps all day and night long, making them can be as much fun as using them. This workshop brings forward this aim of working out a way to make mobile application development a fun experience and a learning endeavor.  ",
+      imageURL: "https://www.linkpicture.com/q/@22posts.png",
+      date: "25-08-2022",
+      registeration: 0,
+  
+    },
+    {
+      name: "Open Summer of Code",
+      description: "OPEN Summer of Code (OSC) is a 30-day open-source initiative that runs from June through July, 2022. At OSC, students would gain knowledge and advance by engaging in the open source community. Participants will assist to some excellent real-world projects through OPEN on GitHub from the leisure of their own homes, guided by mentors and competent experts.The projects would be structured in such a manner that all types of contributors, from newbie teething troubles and documentation to experts and advanced problems, will be able to contribute. Furthermore, we would offer many mind-calming technical activities each weekend.",
+      imageURL: "https://www.linkpicture.com/q/osoc.jpg",
+      date: "15-06-2022",
+      registeration: 0,
+    },
+    {
+      name: "Introduction to RPA and UiPath",
+      description: "Uncovering RPA and UiPath is a one-day workshop with the ambition of introducing one of the trendiest technologies in the tech gala. Robotic Process Automation is a technology that helps in furnishing the businesses with “bots” or software robots that pluck out the mundane tasks from the system and mimic the actions of a human worker to finish them. This is a rapidly evolving field which carries vast potential in transforming the way we work by improving efficiency. This event will start from the basic introduction of RPA, including details of its functioning, its benefits and applications. Learning about the various fields where RPA functions as a blooming industry will help one understand the importance automation carries in the future. Being equipped with the knowledge about the automation world will help in being industry ready. This workshop strives to go forward with the vision of making everybody automation ready.",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1Diy9JVDH21LpCEl93Ox2qbAatH5VI7Mz",
+      date: "22-11-2021",
+      registeration: 0,
+    },
+    {
+      name: "In the Loop",
+      description: "In the loop, a technical coding event, that is being conducted on Hackerrank to make you familiar with the dos and don'ts of competitive coding that you might require for your future ventures.",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1iDSeWpiDfQIxrNlfueOj1TF6ixpY6H26",
+      date: "31-10-2021",
+      registeration: 0,
+    },
+    {
+      name: "How to build your career in DevOps",
+      description: "Enhance your knowledge in the field and scope of DevOps",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1nNKTOYyBT33tyCcyYirV2JqUrk8G9Nau",
+      date: "10-10-2021",
+      registeration: 0,
+    },
+    {
+      name: "Orientation Session for new members",
+      description: "Introduction of the new members with the team",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1bDibJfwP1kQbEZbnjZKCZx4TZ0AUDDA5",
+      date: "05-09-2021",
+      registeration: 0,
+    },
+    {
+      name: "Recruitment drive 2k21",
+      description: "Our Community is pleased to declare that we welcome you to be an integral part of the core committee of trustees and more significantly, our family.",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1NB_EfaSHOWaDpoq_WwrKtKnafNL44lL7",
+  
+      date: "28-08-2021",
+      registeration: 0,
+    },
+    {
+      name: "Webinar on “Career- The little known ways to success” ",
+      description: "To provide tips and tricks regarding resume building explaining the importance of porjects and internships",
+      imageURL:
+        "https://drive.google.com/uc?export=view&id=1r1AkKZYWv3c5C5qngYlmB7aMQdf-sZ1J",
+      date: "19-06-2021",
+      registeration: 0,
+    },
+    {
+      name: "AI & Deep learning webinar and hands on session",
+      description: "Enhance your knowledge about deep learning in industries along with implementation of machine learning",
+      imageURL: "",
+      date: "28-02-2021",
+      registeration: 0,
+    },
+    {
+      name: "Open Source Drive",
+      decription: "Foster the open source compliance along with hands on sessions",
+      imageURL: "",
+      date: "25-10-2020",
+      registeration: 0,
+    },
+    {
+      name: "Session on OPEN Source by team collaboration",
+      description: "Understanding the roles and responsibilities and development of projects",
+      imageURL: "",
+      date: "06-02-2020",
+      registeration: 0,
+    },
+    {
+      name: "Webinar on blockchain",
+      description: "Need and implementation of blockchain technology",
+      imageURL: "",
+      date: "19-09-2020",
+      registeration: 0,
+    },
+    {
+      name: "Digital Marketing 360",
+      description: "explore digital marketing and its need in these times By Ms Alfeeyaa Shaikh, Training Specialist, Here Technologies",
+      imageURL: "",
+      date: "06-07-2020",
+      registeration: 0,
+    },
+    {
+      name: "OPEN source technologies for IOT session",
+      description: " elore digital marketing and its need in these times By Ms Alfeeyaa Shaikh, Training Specialist, Here Technologies",
+      imageURL: "",
+      date: "06-07-2020",
+      registeration: 0,
+    },
+  ];
+  
