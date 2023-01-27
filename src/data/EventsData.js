f096eb511da541a0ad317b4a5f99d6078a4e63c9
Poorva <poorvamaggu2003@gmail.com>
2023-01-27 20:35:50 +0530

    events data updated

--- src/data/EventsData.js
+++ src/data/EventsData.js
@@ -1,7 +1,7 @@
 export const EventsData = [
     {
       name: "Uncovering RPA",
-      description: "Uncovering RPA and UiPath is a one-day workshop with the ambition of introducing one of the trendiest technologies in the tech gala. Robotic Process Automation is a technology that helps in furnishing the businesses with “bots” or software robots that pluck out the mundane tasks from the system and mimic the actions of a human worker to finish them. This is a rapidly evolving field which carries vast potential in transforming the way we work by improving efficiency. This event will start from the basic introduction of RPA, including details of its functioning, its benefits and applications. Learning about the various fields where RPA functions as a blooming industry will help one understand the importance automation carries in the future. Being equipped with the knowledge about the automation world will help in being industry ready. This workshop strives to go forward with the vision of making everybody automation ready. ",
+      description: "Uncovering RPA and UiPath is a one-day workshop with the ambition of introducing one of the trendiest technologies in the tech gala. Robotic Process Automation is a technology that helps in furnishing the businesses with “bots” or software robots that pluck out the mundane tasks from the system and mimic the actions of a human worker to finish them. This event will start from the basic introduction of RPA, including details of its functioning, its benefits and applications. Learning about the various fields where RPA functions as a blooming industry will help one understand the importance automation carries in the future.",
       imageURL:
         "https://i.postimg.cc/tCCZY2CH/Whats-App-Image-2023-01-19-at-10-38-36-PM.jpg",
       date: "23-01-2023",
@@ -9,30 +9,22 @@ export const EventsData = [
     },
     {
       name: "GIT Workshop and hands-on session",
-      description: "GIT is an open source distributed version control system that helps developers track changes in their code. In this workshop, participants will learn how to install and use GIT, how to create repositories, manage branches, and collaborate on projects using GIT. They will also learn how to use the command line interface and other advanced features. This workshop is ideal for beginning developers who want to learn about version control systems but also for experienced developers who want to become more proficient in using GIT.By attending this workshop, participants will walk away with a better understanding of GIT and its use in software development processes. They will be able to confidently use GIT in their own project or team environments to facilitate collaboration, version control, and open source development.",
-      imageURL: "https://ibb.co/TtX82pd",
+      description: "OPEN Community is back with a hands-on workshop on GitHub from scratch and that too, completely free but you know, you have a chance to grab the exciting prizes and appreciation certificate from the HOD sir.We'll be joined by Sanyam Mehediratta and Raghav Agarwal who are Lead developers of OPEN community.We have some Amazon vouchers and other exciting prizes (worth rupees 2500 INR) also, which will be provided based on small tasks performance during the workshop.By attending this workshop, participants will walk away with a better understanding of GIT and its use in software development processes.",
+      imageURL: "https://i.postimg.cc/Gt06w6BY/513-A9629-81-AB-44-BE-817-B-E5-DFE9-A77-C11.jpg",
       date: "18-01-2023",
       registeration: 0,
     },
     {
       name: "Dehashed",
-      description: "Dehashed is a one-day workshop with the aim to enlighten you about the new technology that can break the ecosystem. It renders as an amazing opportunity to get familiar with Blockchain and also get all your queries about NFTs, Crypto, Web 3.0 and Filecoin answered. It will not only strengthen your resume but will also help you to dive vastly in the sea of Blockchain Technology. It also offers an incomparable  chance of getting your contribution recognized and gaining your own digital asset. A thrilling opportunity to encounter a learning experience that can be helpful beyond words. Blockchain is the technology of today, making it an impactful tool for any new endures. Partnering with WEB CRUX along with being sponsored by the Filecoin Foundation, OPEN offers a fortuity like never before. Being vigilant about metaverse and cryptocurrencies is the need of the hour, offering it in such a way that they are not only easily understood but are also comprehended with their practical  utility is like a boon. Dehashed is just the place to retort all your questions.",
+      description: "Dehashed is a one-day workshop with the aim to enlighten you about the new technology that can break the ecosystem that is Blockchain and also get all your queries about NFTs, Crypto, Web 3.0 and Filecoin answered. Blockchain is the technology of today, making it an impactful tool for any new endures. Partnering with WEB CRUX along with being sponsored by the Filecoin Foundation. Being vigilant about metaverse and cryptocurrencies is the need of the hour, offering it in such a way that they are not only easily understood but are also comprehended with their practical utility is like a boon.",
       imageURL:
         "https://i.postimg.cc/SNbp9Tk9/dehashed.jpg",
       date: "07-11-2022",
       registeration: 0,
     },
     {
-      name: "Registration Drive 2022-23",
-      description: "Our Community is pleased to declare that we welcome you to be an integral part of the core committee of trustees and more significantly, our family.",
-      imageURL:
-        "https://drive.google.com/uc?export=view&id=1OszHe35ADNfCMxnPCawwziJ0C7L5uwDP",
-      date: "17-09-2022",
-      registeration: 0,
-    },
-    {
       name: "Flutter Workshop",
-      description: "Flutter is a two-day long workshop with the aim of equipping all with the necessary tools to create hybrid apps. It not only helps in refining one’s technical skills but also helps to harness all the basic dialogues that need to be checked to learn to do hands-on projects. Also, it might just be the kickstart you needed to begin your mobile application development journey. This workshop aims to furnish you with all the skills you need to push all your boundaries and exceed your own expectations. It will help to learn everything from scratch and also provide hands-on experience on each and every aspect. We surround ourselves with mobile apps all day and night long, making them can be as much fun as using them. This workshop brings forward this aim of working out a way to make mobile application development a fun experience and a learning endeavor.  ",
+      description: "Flutter is a two-day long workshop with the aim of equipping all with the necessary tools to create hybrid apps. It helps in refining one’s technical skills and helps to harness all the basic dialogues that need to be checked to learn to do hands-on projects. This workshop aims to furnish you with all the skills you need to push all your boundaries and exceed your own expectations. It will help to learn everything from scratch and also provide hands-on experience on each and every aspect. We surround ourselves with mobile apps all day and night long, making them can be as much fun as using them. ",
       imageURL: "https://www.linkpicture.com/q/@22posts.png",
       date: "25-08-2022",
       registeration: 0,
@@ -55,7 +47,7 @@ export const EventsData = [
     },
     {
       name: "In the Loop",
-      description: "In the loop, a technical coding event, that is being conducted on Hackerrank to make you familiar with the dos and don'ts of competitive coding that you might require for your future ventures.",
+      description: "Competitive coding is the new and most effective way for problem-solving. It is a mental sport where coders code according to a given constraint. OPEN-Community is proud to present to you, In the loop, a technical coding event, that is being conducted on Hackerrank to make you familiar with the dos and don'ts of competitive coding that you might require for your future ventures. This event will feature coding challenges and competitions for all skill levels. It's a great opportunity to test your coding skills, learn new technologies and compete with other coders.",
       imageURL:
         "https://drive.google.com/uc?export=view&id=1iDSeWpiDfQIxrNlfueOj1TF6ixpY6H26",
       date: "31-10-2021",
@@ -63,32 +55,15 @@ export const EventsData = [
     },
     {
       name: "How to build your career in DevOps",
-      description: "Enhance your knowledge in the field and scope of DevOps",
+      description: "OPEN Community is back yet again for you all with an insightful webinar on how to build a career on DevOps. We will be joined by the eloquent speaker Savinder Puri, he has sheer knowledge and experience in the field of DevOps and is currently working as the Global head of Agile, DevSecOps, and cloud platform solutions.This workshop will cover the latest techniques and tools used in the industry to streamline software development and IT operations. Whether you're a beginner or an experienced professional, this workshop will provide valuable insights and hands-on experience. ",
       imageURL:
         "https://drive.google.com/uc?export=view&id=1nNKTOYyBT33tyCcyYirV2JqUrk8G9Nau",
       date: "10-10-2021",
       registeration: 0,
     },
     {
-      name: "Orientation Session for new members",
-      description: "Introduction of the new members with the team",
-      imageURL:
-        "https://drive.google.com/uc?export=view&id=1bDibJfwP1kQbEZbnjZKCZx4TZ0AUDDA5",
-      date: "05-09-2021",
-      registeration: 0,
-    },
-    {
-      name: "Recruitment drive 2k21",
-      description: "Our Community is pleased to declare that we welcome you to be an integral part of the core committee of trustees and more significantly, our family.",
-      imageURL:
-        "https://drive.google.com/uc?export=view&id=1NB_EfaSHOWaDpoq_WwrKtKnafNL44lL7",
-  
-      date: "28-08-2021",
-      registeration: 0,
-    },
-    {
       name: "Webinar on “Career- The little known ways to success” ",
-      description: "To provide tips and tricks regarding resume building explaining the importance of porjects and internships",
+      description: "OPEN brings you a session to answer all your doubts and to provide you with the best tips regarding resume building, Linkedin profile building, insights of various minor and major projects, internships and many other opportunities.Career- The Little Known Ways to Success a webinar aimed to answer various queries of students regarding resume building and giving their careers a boost in the right direction.Join this interactive session with Mr. Mridul Thapa (Business Analyst at Merkle Sokrati and explore various ways in which you can bring out the best in you.This webinar will explore little-known ways to achieve success in your career.",
       imageURL:
         "https://drive.google.com/uc?export=view&id=1r1AkKZYWv3c5C5qngYlmB7aMQdf-sZ1J",
       date: "19-06-2021",
@@ -96,43 +71,29 @@ export const EventsData = [
     },
     {
       name: "AI & Deep learning webinar and hands on session",
-      description: "Enhance your knowledge about deep learning in industries along with implementation of machine learning",
-      imageURL: "",
+      description: "Chat GPT, Character AI, Open AI and others revolving around your mind all the time. The OPEN Community is thrilled to announce an upcoming webinar on Deep Learning in AI. The webinar will be your best answer to all AI-related questions and opportunities. The speaker, Etqad Ahmad Khan, a Data Scientist at Ericsson and a UPES alumnus, will pay attention to creating better learning opportunities and making learning fun by providing a hands-on experience rather than conventional learning. We certainly hope that the session will be valuable for all AI enthusiasts.",
+      imageURL: "https://i.postimg.cc/d0syn04Z/76785-F98-FC8-D-4718-B3-C1-201-D5-F1-CAFA2.jpg",
       date: "28-02-2021",
       registeration: 0,
     },
     {
-      name: "Open Source Drive",
-      decription: "Foster the open source compliance along with hands on sessions",
-      imageURL: "",
-      date: "25-10-2020",
-      registeration: 0,
-    },
-    {
-      name: "Session on OPEN Source by team collaboration",
-      description: "Understanding the roles and responsibilities and development of projects",
-      imageURL: "",
-      date: "06-02-2020",
-      registeration: 0,
-    },
-    {
       name: "Webinar on blockchain",
-      description: "Need and implementation of blockchain technology",
-      imageURL: "",
+      description: "The webinar will be an opportunity for attendees to gain a deeper understanding of blockchain, its uses, and its potential impact on various industries. Our expert speaker will cover the basics of blockchain, its underlying technology, and its real-world applications. Understand everything about this technology by participating in this event and get a firm hold on this topic.By attending this webinar, attendees will be better equipped to understand the potential uses of blockchain technology and industrial application. They will also be able to network with other professionals and explore potential career opportunities in the field.",
+      imageURL: "https://i.postimg.cc/mgDzKg0v/A9-DE5301-B632-4-DE2-BF2-F-73-A41-F524-D4-A.jpg",
       date: "19-09-2020",
       registeration: 0,
     },
     {
       name: "Digital Marketing 360",
-      description: "explore digital marketing and its need in these times By Ms Alfeeyaa Shaikh, Training Specialist, Here Technologies",
-      imageURL: "",
+      description: "OPEN Community in collaboration with UPES Council for Innovation and Entrepreneurship, presents to you a webinar: Digital Marketing 360. Attendees will learn about the various digital marketing channels available and how to use them effectively. The expert speaker, Ms. Shashwati Shubhadarshini, Senior Business Analyst, Merkle Sokrati will discuss topics such as search engine optimization, email marketing, content marketing, and more. This webinar is ideal for professionals looking to expand their knowledge of digital marketing and for those who want to learn about the latest trends and best practices in the field.",
+      imageURL: "https://i.postimg.cc/C1dc10BX/EFDBE454-E64-B-435-F-B710-EAD0-E9-BE3-F7-B.jpg",
       date: "06-07-2020",
       registeration: 0,
     },
     {
       name: "OPEN source technologies for IOT session",
-      description: " elore digital marketing and its need in these times By Ms Alfeeyaa Shaikh, Training Specialist, Here Technologies",
-      imageURL: "",
+      description: "OPEN and IOT go hand-in-hand provided a path by Dev-Ops. This integration is the exact demand of the future. With this idea in mind, OPEN will be organizing an Open Source Technologies for IoT session with a guest speaker, Mr. Manoj Mittal from Blue Rose Technologies. Mr. Mittal has also been a part of Sopra Steria, Convergys, and Infozech Software Private Limited in the past years. He will be communicating with aspiring engineers and describing the extensive use of open-source technology in IoT in the DevOps era.",
+      imageURL: "https://i.postimg.cc/PfwMF2mW/2-ABC0682-D50-A-4-CB3-A9-E9-B9-C9-F25-BA86-F.jpg",
       date: "06-07-2020",
       registeration: 0,
     },
