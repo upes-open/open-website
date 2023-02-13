e0f44dd8bf6acb0dc6ea84f03fd48f91bf6b4361
Akash Panwar <akashpanwar9976@gmail.com>
2023-02-13 20:44:04 +0530

    Detailed upcoming event pages

---
+++ src/pages/events/upcomingDetailed/UpcomingDetailedAnniversary.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+
+// import dehashed_img_1 from "../../../../assets/img/events/dehashed/dehashed_img_1.jpg";
+// import dehashed_img_2 from "../../../../assets/img/events/dehashed/dehashed_img_2.jpg";
+// import dehashed_img_3 from "../../../../assets/img/events/dehashed/dehashed_img_3.jpg";
+// import dehashed_img_4 from "../../../../assets/img/events/dehashed/dehashed_img_4.jpg";
+// import dehashed_img_5 from "../../../../assets/img/events/dehashed/dehashed_img_5.jpg";
+// import dehashed_img_6 from "../../../../assets/img/events/dehashed/dehashed_img_6.jpg";
+// import dehashed_img_7 from "../../../../assets/img/events/dehashed/dehashed_img_7.jpg";
+// import dehashed_img_9 from "../../../../assets/img/events/dehashed/dehashed_img_8.jpg";
+
+function SingleArea() {
+  return (
+    <section className="game-single-area pt-120 pb-120">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="game-single-content">
+              <div className="upcoming-game-head text-center">
+                <div className="uc-game-head-title">
+                  <span>01-02-2023</span>
+                  <h4>OPEN'S 4th Anniversary</h4>
+                </div>
+                <div className="uc-game-price">
+                  <h5>Welcome to OPEN!</h5>
+                </div>
+              </div>
+              <div className="game-single-img d-flex justify-content-center">
+                <img
+                  height="450px"
+                  src="https://i.postimg.cc/pL2cJGBk/15106-C4-D-0225-40-F9-8-E7-E-C126630-D6-DAC.png"
+                  className="wow fadeInRight"
+                  data-wow-delay=".3s"
+                  alt=""
+                />
+              </div>
+              <p className="mt-20 text-center">
+                <span>OPEN</span> Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Quae error, iure, temporibus unde enim
+                incidunt libero corrupti laboriosam veritatis quasi dicta
+                pariatur minima suscipit. Quibusdam optio non ut hic aliquam.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error
+                adipisci ab aperiam distinctio at asperiores eligendi ipsa porro
+                illum! Quasi. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Atque, veniam!{" "}
+              </p>
+              <p className="text-center">
+                It will not only strengthen your resume but will also help you
+                to dive vastly in the sea of Blockchain Technology. It also
+                offers an incomparable chance of getting your contribution
+                recognized and gaining your own digital asset. A thrilling
+                opportunity to encounter a learning experience that can be
+                helpful beyond words. Blockchain is the technology of today,
+                making it an impactful tool for any new endures. Partnering with
+                WEB CRUX along with being sponsored by the Filecoin Foundation,
+                OPEN offers a fortuity like never before. Being vigilant about
+                metaverse and cryptocurrencies is the need of the hour, offering
+                it in such a way that they are not only easily understood but
+                are also comprehended with their practical utility is like a
+                boon. Dehashed is just the place to retort all your questions.
+              </p>
+
+              <div class="container">
+                <div class="row">
+                  <div class="col">
+                    <div className="game-single-title mt-60 mb-30">
+                      <h4>Speaker details</h4>
+                    </div>
+                    <div className="game-single-info mb-65">
+                      <ul>
+                        <li>
+                          <span>Speaker :</span> Akash Panwar
+                        </li>
+                        <li>
+                          <span>Achievements :</span> Lorem, ipsum dolor sit
+                          amet consectetur.
+                        </li>
+                        <li>
+                          <span>LinkedIn :</span>{" "}
+                          <a href="https://www.linkedin.com/in/akash-panwar-27b0141bb/">
+                            Click here
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div className="game-single-title mt-60 mb-30">
+                      <h4>Venue details</h4>
+                    </div>
+                    <div className="game-single-info mb-65">
+                      <ul>
+                        <li>
+                          <span>Venue :</span> UPES
+                        </li>
+                        <li>
+                          <span>Number of attendees :</span> 187
+                        </li>
+                        <li>
+                          <span>XYZ :</span> 187
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* <div className="game-single-title mb-30">
+                <h4>
+                  Read <span>More ...</span>
+                </h4>
+              </div>
+              <p>
+                Dehashed workshop endured monumental success in bringing over
+                550 registrations and more than 100 attendees who learned with
+                zeal and made this workshop a fruitful success. All the 4
+                speakers did an outstanding job narrating their expertise that
+                made learning a fun ground. This workshop introduced blockchain
+                to everyone, reducing the entry barrier by practically helping
+                the participants in creating their own web 3.0 wallet , over 78
+                web 3.0 wallets were made till the end. Introducing the
+                attendees to cryptocurrencies and development networks by giving
+                them tokens to perform transactions helped them endure real
+                world implementation of blockchain. NFTs and metaverse were also
+                instigated by explaining about their utilities and also helping
+                them in buying their own NFTs from the collection that was made
+                for the event. The attendees were also taught how to mint them
+                and list them on the global NFT marketplace. Over 200 NFTs were
+                minted in the process and the expenditure on crypto was worth
+                more than 4000. All the credit for making this workshop a
+                success goes out to all the people who contributed their best
+                and also to those who brought an open and welcoming learning
+                mind. This workshop surely enlightened the learning journey of
+                all and also paved a way for being open to learn more about the
+                newest technologies.
+              </p>
+              <div className="game-single-title mt-30 mb-10">
+                <h4>Glimpses</h4>
+              </div>
+              <div className="game-single-gallery">
+                <div className="row">
+                  <div className="col-lg-4 col-sm-6 mb-30">
+                    <img src={dehashed_img_1} className="h-100" alt="" />
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-30">
+                    <img src={dehashed_img_2} className="h-100" alt="" />
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-30">
+                    <img src={dehashed_img_3} className="h-100" alt="" />
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-30">
+                    <img src={dehashed_img_4} className="h-100" alt="" />
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-30">
+                    <img src={dehashed_img_5} className="h-100" alt="" />
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-30">
+                    <img src={dehashed_img_6} className="h-100" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className="game-single-shape">
+                <img src="assets/img/images/game_section_shape.png" alt="" />
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SingleArea;
