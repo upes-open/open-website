e176697d24a54eb11b36c38c6ecc315b59755fb4
avnigoyal25 <500090996@stu.upes.ac.in>
2024-07-24 21:17:53 +0530

    registratin form completed

--- src/pages/register/OpenRegister.jsx
+++ src/pages/register/OpenRegister.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useState } from 'react';
-import axios from 'axios';
 import ReCAPTCHA from "react-google-recaptcha";
 import '../../assets/css/register.css';
 import Modal from 'react-modal';
 import { useForm} from "react-hook-form"
+import { database} from "../firebase/firebase";
+import { set,ref } from "firebase/database";
+import toast, { Toaster } from 'react-hot-toast';
 
 Modal.setAppElement('#root');
 
 function OpenRegister() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   function onChange(props) {
     console.log("Captcha value:", props);
   }
@@ -17,28 +20,58 @@ function OpenRegister() {
     event.preventDefault();
     setModalIsOpen(true);
   };
+
   const {
     register,
     handleSubmit,
+    reset,
     watch,
     formState: { errors },
   } = useForm()
 
   const onSubmit = (data) => {
-    const vertical = [];
-    if (data.engagement) vertical.push("engagement");
-    if (data.collaboration) vertical.push("collaboration");
-    if (data.marketing) vertical.push("marketing");
+    setIsSubmitting(true);
+    const verticals = [];
+    if (data.engagement) verticals.push("engagement");
+    if (data.collaboration) verticals.push("collaboration");
+    if (data.marketing) verticals.push("marketing");
 
-    const processedData = { ...data, vertical };
+    const teams = [];
+    if (data["student-developer"]) teams.push("Student Developer");
+    if (data.photography) teams.push("Photography");
+    if (data.design) teams.push("Design");
+    if (data.PR) teams.push("PR Team");
+    if (data["social-media"]) teams.push("Social Media Team");
+
+    const processedData = { ...data, verticals, teams };
     delete processedData.engagement;
     delete processedData.collaboration;
     delete processedData.marketing;
-    console.log(processedData)
-  }
+    delete processedData["student-developer"];
+    delete processedData.photography;
+    delete processedData.design;
+    delete processedData.PR;
+    delete processedData["social-media"];
 
+    console.log(processedData);
+    
+    const reference = ref(database, 'registrations/' + data.name); 
+    set(reference, processedData)
+      .then(() => {
+        console.log("Data successfully sent to Firebase");
+        reset();
+        toast.success('Registration successfull!')
+        setIsSubmitting(false);
+      })
+      .catch((error) => {
+        console.error("Error sending data to Firebase", error);
+        toast.error(`${error}`)
+        setIsSubmitting(false);
+      });
+  };
   return (
     <section className="contact-area pt-120 pb-120">
+      <div><Toaster /></div>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-6 pl-45">
@@ -83,7 +116,7 @@ function OpenRegister() {
                   />
                 </div>
                 <div className="row">
-                  <input type="number" placeholder="Your SAP ID" required {...register("sap")}/>
+                  <input placeholder="Your SAP ID" required {...register("sap")}/>
                 </div>
                 <div className="row">
                   <input type="text" placeholder="Your Branch" required {...register("branch")}/>
@@ -92,21 +125,34 @@ function OpenRegister() {
                   <input type="text" placeholder="Your Course" required {...register("course")}/>
                 </div>
                 <div className="row">
-                  <input type="number" placeholder="Current year" required {...register("number")}/>
+                  <input placeholder="Current year" required {...register("number")}/>
                 </div>
                 <div className="row">
-                  <input type="date" placeholder="Birth Date" required {...register("birth-date")}/>
+                  <span>Your birth date:</span><input type="date" placeholder="Birth Date" required {...register("birth-date")}/>
                 </div>
                 <div id="div2">
                   <p style={{ color: '#7E7E7E', width: '350px' }}> <a href="#" style={{color:'red'}} onClick={openModal}>Click here</a> to know about different verticals</p>
                   <p style={{ color: '#7E7E7E' }}>Interested Vertical</p>
-                  <input id="input2" type="checkbox" {...register("engagement")}/> <label id="label2">Engagement</label>
+                  <input id="input2" type="checkbox" {...register("engagement")} /> <label id="label2">Engagement</label>
                   <br />
-                  <input id="input2" type="checkbox" {...register("collaboration")}/> <label >Collaboration</label>
+                  <input id="input2" type="checkbox" {...register("collaboration")} /> <label >Collaboration</label>
                   <br />
-                  <input id="input2" type="checkbox" {...register("marketting")}/> <label>Marketing</label>
+                  <input id="input2" type="checkbox" {...register("marketing")} /> <label>Marketing</label>
                 </div>
                 <br />
+                <div id="div2">
+                  <p style={{ color: '#7E7E7E' }}>Interested team</p>
+                  <input id="input2" type="checkbox" {...register("student-developer")} /> <label id="label2" style={{width:'250px'}}>Student Developer(Collaboration vertical)</label>
+                  <br />
+                  <input id="input2" type="checkbox" {...register("photography")} /> <label style={{width:'250px'}}>Photography (Engagement vertical)</label>
+                  <br />
+                  <input id="input2" type="checkbox" {...register("design")} /> <label style={{width:'250px'}}>Design (Engagement vertical)</label>
+                  <br />
+                  <input id="input2" type="checkbox" {...register("PR")} /> <label style={{width:'250px'}}>PR Team (Marketing vertical)</label>
+                  <br />
+                  <input id="input2" type="checkbox" {...register("social-media")} /> <label style={{width:'250px'}}>Social Media Team (Marketing vertical)</label>
+                </div>
+                <br/>
                 <div className="row">
                   <input type="text" placeholder="Resume/Portfolio link" required {...register("resume")}/>
                 </div>
@@ -122,7 +168,9 @@ function OpenRegister() {
                 <br />
                 <br />
                 <div className="row justify-content-center">
-                  <button type="submit" className="register btn btn-style-two">Register</button>
+                <button type="submit" className="register btn btn-style-two" disabled={isSubmitting} style={{color:'black'}}>
+                    {isSubmitting ? 'Submitting...' : 'Register'}
+                  </button>
                 </div>
               </form>
               <span id="msg"></span>
@@ -150,7 +198,7 @@ function OpenRegister() {
       >
         <button onClick={() => setModalIsOpen(false)} style={{ float: 'right',cursor:'pointer',color:'red' }}>X</button>
         <h2 style={{color:'black'}}>Different Verticals</h2>
-        <img src="https://i.postimg.cc/T3nL2LVc/Screenshot-2024-07-21-152319.png" alt="Verticals" style={{ width: '100%' }} />
+        <img src="https://i.postimg.cc/DzGpSY6q/Screenshot-2024-07-24-173448.png" alt="Verticals" style={{ width: '100%' }} />
       </Modal>
     </section>
   );
