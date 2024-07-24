3ee0166604ec8a32314ad6e178f055c2e177d212
Akanksha Gupta <64099160+akanksha2305@users.noreply.github.com>
2024-07-24 23:52:14 +0530

    Update firebase.js

--- src/pages/firebase/firebase.js
+++ src/pages/firebase/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database"; 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCBRP_et_Sg44bXes0snM6O_S1AV2-uUsA",
+    authDomain: "upes-open.firebaseapp.com",
+    databaseURL: "https://upes-open-default-rtdb.firebaseio.com",
+    projectId: "upes-open",
+    storageBucket: "upes-open.appspot.com",
+    messagingSenderId: "924316504730",
+    appId: "1:924316504730:web:bbec97b57330b02c274523",
+    measurementId: "G-VHDNH0E7T8"
 };
 
 // Initialize Firebase
