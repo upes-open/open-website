f15b1a62451c8eb692e8818237d7d2492c932683
Akanksha Gupta <64099160+akanksha2305@users.noreply.github.com>
2025-01-11 13:55:06 +0530

    Update RegisterRedirect.js

--- src/components/RegisterRedirect.js
+++ src/components/RegisterRedirect.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 function RegisterRedirect() {
   useEffect(() => {
-    window.location.href = "https://upes-open.org/valorant_register";
+    window.location.href = "https://gdg.community.dev/events/details/google-gdg-on-campus-university-of-petroleum-and-energy-studies-dehradun-india-presents-spectrum-the-design-workshop/";
   }, []);
 
   return null; // Since we are redirecting, no need to render anything
