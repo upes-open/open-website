64853c587bc6790fe47301a10ff6979d1756556d
Akanksha Gupta <64099160+akanksha2305@users.noreply.github.com>
2025-01-11 13:53:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ function App() {
             </SingleEvent>
           </ScrollToTopRoute>
           <Route exact path="/register">
-           <Redirect to="https://upes-open.org/valorant_register" />
+           <Redirect to="https://gdg.community.dev/events/details/google-gdg-on-campus-university-of-petroleum-and-energy-studies-dehradun-india-presents-spectrum-the-design-workshop/" />
             <RegisterRedirect />
           </Route>
           <ScrollToTopRoute exact path="/codeyourfuture">
